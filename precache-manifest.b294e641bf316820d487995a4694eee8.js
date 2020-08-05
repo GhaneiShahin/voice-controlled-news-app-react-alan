942c57a3cd348d386cf3c01a2b3d1c066c475002
ShahinGhanei94 <shahinghanei94@sh94gh.local>
2020-08-05 17:58:38 +0200

    Updates

---
+++ precache-manifest.b294e641bf316820d487995a4694eee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5d40b184ce8bb233a0cd9d3ce16780",
+    "url": "/voice-controlled-news-app-react-alan/index.html"
+  },
+  {
+    "revision": "5439956b9d8560f26dbe",
+    "url": "/voice-controlled-news-app-react-alan/static/css/main.dddf91e4.chunk.css"
+  },
+  {
+    "revision": "286c67826b292d26033b",
+    "url": "/voice-controlled-news-app-react-alan/static/js/2.cdb68b6b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/voice-controlled-news-app-react-alan/static/js/2.cdb68b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5439956b9d8560f26dbe",
+    "url": "/voice-controlled-news-app-react-alan/static/js/main.f93fd1cf.chunk.js"
+  },
+  {
+    "revision": "72b9fb2291fc598222e3",
+    "url": "/voice-controlled-news-app-react-alan/static/js/runtime-main.db057005.js"
+  }
+]);
